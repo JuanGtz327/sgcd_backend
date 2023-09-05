22431931876b4461e1c54a22dd5455e26c7331e7
Juan Carlos <jucasaguipn09@gmail.com>
2023-09-05 00:01:45 -0600

    All configurations succeded

--- src/routes/router.js
+++ src/routes/router.js
@@ -5,19 +5,10 @@ import {login,signup,logout,profile} from '../controllers/auth.controller.js'
 import {authRequired} from '../middlewares/validateToken.js'
 
 import Admin from "../models/admin.js";
-import sequelize from '../db.js'
 
 router.get("/", async (req, res) => {
-  //const admins = await Admin.findAll();
-  //res.status(200).send(JSON.stringify(admins,null,2));
-  //res.status(200).send(`ENV ${JSON.stringify(process.env,null,2)}`);
-  try {
-    await sequelize.authenticate();
-    return res.send("DB is connected")
-  } catch (error) {
-    return res.send(`BD ERROR ${error}`);
-  }
-  
+  const admins = await Admin.findAll();
+  res.status(200).send(JSON.stringify(admins,null,2));
 });
 
 router.post("/login",login);
